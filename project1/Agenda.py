c83ba82182b1ce4f8493818db91266df93e4d71c
aumerinop <aumerino@uc.cl>
2013-09-04 12:12:58 -0400

    Update Agenda.py

--- project1/Agenda.py
+++ project1/Agenda.py
@@ -1,9 +1,11 @@
 class Agenda(Proceso.Proceso):
 
-    def __init__(self,nombre,tiempo,nombre_contacto,numero):
+    def __init__(self,nombre,fecha,tipo_proceso.prioridad,nombre_contacto,numero):
     	self.id = id
     	self.name = nombre
-    	self.time = tiempo
+    	self.date = fecha
+    	self.tipo = tipo_proceso
+    	self.priority = prioridad
     	self.contact = nombre_contacto
     	self.number = numero
 
