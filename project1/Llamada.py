40e511095fbe3fa45df3dc6231b206b16297f2e1
aumerinop <aumerino@uc.cl>
2013-09-04 10:49:13 -0400

    Update Llamada.py

---
+++ project1/Llamada.py
@@ -0,0 +1,16 @@
+import Proceso
+
+class Llamada(Proceso.Proceso):
+
+    def __init__(self,nombre,tiempo,numero,duracion):
+        self.id = id
+        self.name = nombre
+        self.time = tiempo
+        self.number = numero
+        self.length = duracion
+
+    def getNumber(self):
+        return self.number
+
+    def getLength(self):
+        return self.length
