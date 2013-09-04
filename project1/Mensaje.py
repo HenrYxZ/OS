fae0a49e4ed179bbfbb28c95db468dde8ec010ab
aumerinop <aumerino@uc.cl>
2013-09-04 12:15:37 -0400

    Update Mensaje.py

--- project1/Mensaje.py
+++ project1/Mensaje.py
@@ -2,10 +2,12 @@ import Proceso
 
 class Mensaje(Proceso.Proceso):
 
-    def __init__(self,nombre,tiempo,numero,texto):
+    def __init__(self,nombre,fecha,tipo_proceso,prioridad,numero,texto):
         self.id = id
         self.name = nombre
-        self.time = tiempo
+        self.date = fecha
+    	self.tipo = tipo_proceso
+    	self.priority = prioridad
         self.number = numero
         self.text = texto
 
