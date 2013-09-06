19802ecfd74ca88031926e807d4c2d15909eb6c4
rnsfeir <rsfeir91@hotmail.com>
2013-09-06 12:13:23 -0400

    Update Parser.py

--- project1/Parser.py
+++ project1/Parser.py
@@ -1,1 +1,15 @@
 # En esta clase se debe poner toda la logica para leer el archivo de entrada
+import Proceso
+
+
+class Parser:
+  
+    def __init__ (self,nombre_archivo):
+        self.n_archivo = nombre_archivo #Inicializamos la clase con el nombre del archivo que leeremos
+  
+  
+    def Leer_Archivo(self):
+    
+    
+    
+  
