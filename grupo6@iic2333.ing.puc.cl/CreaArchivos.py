453ff4f4cbcc05f6b49865cda682fffcf1575a17
HenrY <hjhenriq@gmail.com>
2013-10-22 11:48:38 -0300

    problema con copia

--- grupo6@iic2333.ing.puc.cl/CreaArchivos.py
+++
@@ -1,7 +0,0 @@
-import os
-
-#for i in range(1,20):
-#	os.system("touch ~/projects/OS/project2/ram/" + str(i) + ".txt")
-
-for i in range(501,800):
-	os.system("touch ~/projects/OS/project2/hd/" + str(i) + ".txt")
